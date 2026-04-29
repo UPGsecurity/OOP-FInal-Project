948ce3e9663e3d265cf728c787618164c54e7f43
DBZ <brainxcb@gmail.com>
2026-04-29 13:09:19 +0500

    Create Fine.java

---
+++ scr/main/java/library/transactions/Fine.java
@@ -0,0 +1,29 @@
+package library.transactions;
+
+public class Fine {
+
+    private double      amount;
+    private BookLending lending;
+
+    private static final double FINE_PER_DAY = 1.0;
+
+    public Fine(BookLending lending) {
+        this.lending = lending;
+        this.amount  = calculateAmount();
+    }
+
+    private double calculateAmount() {
+        long overdueDays = lending.getOverdueDays();
+        if (overdueDays <= 0) return 0.0;
+        double calc = overdueDays * FINE_PER_DAY;
+        System.out.println("[FINE] " + overdueDays + " days overdue. Fine: $" + calc);
+        return calc;
+    }
+
+    public double getAmount() { return amount; }
+
+    @Override
+    public String toString() {
+        return "Fine{amount=$" + amount + ", book='" + lending.getBookItem().getTitle() + "'}";
+    }
+}
