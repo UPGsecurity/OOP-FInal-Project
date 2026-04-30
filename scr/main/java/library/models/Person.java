d0699a26a52dcc143d17aa8a528cee19f2752160
DBZ <brainxcb@gmail.com>
2026-04-30 10:57:00 +0500

    Update Person.java

--- scr/main/java/library/models/Person.java
+++ scr/main/java/library/models/Person.java
@@ -1,27 +1,49 @@
 package library.models;
 
-public abstract class Person {
+public class Person {
     protected String name;
     protected Address address;
     protected String email;
     protected String phone;
 
     public Person(String name, Address address, String email, String phone) {
-        this.name    = name;
+        this.name = name;
         this.address = address;
-        this.email   = email;
-        this.phone   = phone;
+        this.email = email;
+        this.phone = phone;
     }
 
-    public String  getName()    { return name; }
-    public Address getAddress() { return address; }
-    public String  getEmail()   { return email; }
-    public String  getPhone()   { return phone; }
+    public String getName() {
+        return name;
+    }
 
-    public void setName(String name)       { this.name = name; }
-    public void setEmail(String email)     { this.email = email; }
-    public void setPhone(String phone)     { this.phone = phone; }
-    public void setAddress(Address address){ this.address = address; }
+    public Address getAddress() {
+        return address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
 
     @Override
     public String toString() {
